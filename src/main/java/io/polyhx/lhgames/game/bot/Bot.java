488834570d8ed15aeefde09ce71aefdeb4a1b07e
Charlie Duquette <charlie_duquette@hotmail.fr>
2018-10-06 14:19:38 -0400

    Collecting mode activated

--- src/main/java/io/polyhx/lhgames/game/bot/Bot.java
+++ src/main/java/io/polyhx/lhgames/game/bot/Bot.java
@@ -1,26 +1,37 @@
 package io.polyhx.lhgames.game.bot;
 
 import io.polyhx.lhgames.game.GameInfo;
-import io.polyhx.lhgames.game.GoTo;
 import io.polyhx.lhgames.game.Map;
 import io.polyhx.lhgames.game.Player;
 import io.polyhx.lhgames.game.action.IAction;
+import io.polyhx.lhgames.game.action.MoveAction;
 import io.polyhx.lhgames.game.point.Point;
-import io.polyhx.lhgames.game.point.VectorPoint;
 import io.polyhx.lhgames.game.tile.ResourceTile;
 
 import java.util.List;
 
-
 public class Bot extends BaseBot {
 
+    int x=4;
 
     public IAction getAction(Map map, Player player, List<Player> others, GameInfo info) {
-        GoTo.mapPrint(map);
+        MoveAction mouvement = null;
 
+        while(x!=0){
+            mouvement = new MoveAction(Point.LEFT);
+            x = x - 1;
+        }
 
-
-
-        return createMoveAction(GoTo.decision(map, player, others, info));
+        return mouvement;
     }
+
+    public boolean IsFull(Player player) {
+        if (player.getCarriedResource() >= player.getResourceCapacity())
+            return true;
+        else
+            return false;
+    }
+
+
+
 }
