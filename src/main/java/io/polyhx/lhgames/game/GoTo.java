79207ef98a00e9cf2c54090d2c358d4ee5c11560
simon <tran.simon2@gmail.com>
2018-10-06 14:38:31 -0400

    COUCOU

--- src/main/java/io/polyhx/lhgames/game/GoTo.java
+++ src/main/java/io/polyhx/lhgames/game/GoTo.java
@@ -1,6 +1,7 @@
 package io.polyhx.lhgames.game;
 
-import io.polyhx.lhgames.game.action.IAction;
+import io.polyhx.lhgames.game.action.*;
+import io.polyhx.lhgames.game.bot.Bot;
 import io.polyhx.lhgames.game.point.IPoint;
 import io.polyhx.lhgames.game.point.Point;
 import io.polyhx.lhgames.game.point.VectorPoint;
@@ -29,7 +30,7 @@ public class GoTo {
                         break;
                     }
                 }
-                if (shouldIgnore) {
+                if (!shouldIgnore) {
                     tile = resTile;
                 }
             }
@@ -48,28 +49,75 @@ public class GoTo {
     }
 
 
-    public static VectorPoint decision(Map map, Player player, List<Player> others, GameInfo info, ArrayList<Point> pointsToIgnore) {
+    public static AbstractPointAction decisionMove(Map map, Player player, List<Player> others, GameInfo info, ArrayList<Point> pointsToIgnore, Point destination) {
+        VectorPoint move = GoTo.goTo(player.getPosition(), destination);
 
+
+        Tile futureTile = map.getTile(VectorPoint.add(player.getPosition(), move));
+
+        if (futureTile.isWall()) {
+            System.err.println("ATTACKING WALL");
+            return new MeleeAttackAction(move);
+//                return decision(map, player, others, info, pointsToIgnore);
+        }
+
+        else if (futureTile.isResource()) {
+            pointsToIgnore.add(futureTile.getPosition());
+            return decisionMove(map, player, others, info, pointsToIgnore, destination);
+        }
+        else if (futureTile.isPlayer()) {
+            return new MeleeAttackAction(move);
+        }
+        else if (futureTile.isLava()) {
+            pointsToIgnore.add(futureTile.getPosition());
+            return decision(map, player, others, info, pointsToIgnore);
+        }
+        else {
+            System.err.println("Going to:" + destination);
+            return new MoveAction(move);
+
+        }
+    }
+
+
+    public static AbstractPointAction decision(Map map, Player player, List<Player> others, GameInfo info, ArrayList<Point> pointsToIgnore) {
+
+        System.out.println(pointsToIgnore);
+        System.out.println("PlayerPos: " + VectorPoint.toString(player.getPosition()));
+        System.out.println("Player Backpack: " + player.getCarriedResource() + " Cap: " + player.getResourceCapacity());
 
         ResourceTile tile = GoTo.getClosestResource(map, player.getPosition(), pointsToIgnore);
+        if (Bot.IsFull(player)) {
+            System.err.println("GOING TO HOME");
+
+            return decisionMove(map, player, others, info, pointsToIgnore, player.getHousePosition());
+        }
         if (tile != null) {
-
-
             VectorPoint move = GoTo.goTo(player.getPosition(), tile.getPosition());
-//        System.out.println("Tile: " + VectorPoint.toString(tile.getPosition()) + " player: " + VectorPoint.toString(player.getPosition()) + " : " + VectorPoint.toString(move));
 
 
-            if (map.getTile(VectorPoint.add(player.getPosition(), move)).isWall()) {
-                pointsToIgnore.add(tile.getPosition());
-                return decision(map, player, others, info, pointsToIgnore);
+            Tile futureTile = map.getTile(VectorPoint.add(player.getPosition(), move));
+
+              if (futureTile.isResource()) {
+                ResourceTile tile12 = null;
+                for (ResourceTile resourceTile : map.getResources()) {
+                    if (VectorPoint.equals(resourceTile.getPosition(), futureTile.getPosition())) {
+                        tile12 = resourceTile;
+                    }
+                }
+                if (tile12 != null) {
+                    System.out.println(tile12.getResource());
+
+                }
+                return new CollectAction(move);
             }
-            else {
-                return move;
-
-            }
+            else{
+                  return decisionMove(map, player, others, info, pointsToIgnore, tile.getPosition());
+              }
         }
         else{
-            return goTo(player.getPosition(), player.getHousePosition());
+            System.err.println("Going HOME");
+            return decisionMove(map, player, others, info, pointsToIgnore, player.getHousePosition());
         }
 
     }
@@ -77,7 +125,7 @@ public class GoTo {
 
     public static void mapPrint(Map map) {
 
-        int sizeX = 25, sizeY = 70;
+        int sizeX = 50, sizeY = 70;
         char[][] mapo = new char[sizeX][sizeY];
 
         for (List<Tile> tiles : map.getTiles()) {
--- src/main/java/io/polyhx/lhgames/game/bot/Bot.java
+++ src/main/java/io/polyhx/lhgames/game/bot/Bot.java
@@ -1,41 +1,36 @@
 package io.polyhx.lhgames.game.bot;
 
 import io.polyhx.lhgames.game.GameInfo;
+import io.polyhx.lhgames.game.GoTo;
 import io.polyhx.lhgames.game.Map;
 import io.polyhx.lhgames.game.Player;
+import io.polyhx.lhgames.game.action.AbstractPointAction;
 import io.polyhx.lhgames.game.action.IAction;
-import io.polyhx.lhgames.game.action.MoveAction;
 import io.polyhx.lhgames.game.point.Point;
+import io.polyhx.lhgames.game.point.VectorPoint;
 import io.polyhx.lhgames.game.tile.ResourceTile;
 
+import java.util.ArrayList;
 import java.util.List;
+
 
 public class Bot extends BaseBot {
 
-    int x=1;
-    int y = 5;
 
     public IAction getAction(Map map, Player player, List<Player> others, GameInfo info) {
-        MoveAction mouvement = null;
+//        GoTo.mapPrint(map);
 
-        if (x!=0){
-            mouvement = new MoveAction(Point.LEFT);
-            x = x - 1;
-        }else if (y!=0){
-            mouvement = new MoveAction(Point.DOWN);
-            y = y - 1;
-        }
 
-        return mouvement;
+        AbstractPointAction action = GoTo.decision(map, player, others, info, new ArrayList<>());
+
+        System.out.println(action.getActionType() + " : "  + action.getJSONContent());
+        return action;
     }
 
-    public boolean IsFull(Player player) {
+    public static boolean IsFull(Player player) {
         if (player.getCarriedResource() >= player.getResourceCapacity())
             return true;
         else
             return false;
     }
-
-
-
 }
